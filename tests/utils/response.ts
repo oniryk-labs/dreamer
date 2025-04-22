351ec1cc7245a08c83e3234bb8c2966975688f0d
brunoziie <eu@brunoziie.com>
2025-04-22 18:25:09 -0300

    test: fix json interceptor for request errors

--- tests/utils/response.ts
+++ tests/utils/response.ts
@@ -1,5 +1,6 @@
 import type { HttpContext, Response } from '@adonisjs/core/http'
 import { CrudVerb } from '../../src/extensions/crud.js'
+import { error } from '../../src/extensions/http.js'
 
 export function interceptJsonResponse(response: Response, onJson: (data: unknown) => void) {
   const originalJson = response.json.bind(response)
@@ -15,16 +16,8 @@ export function executeCrudVerb<V extends CrudVerb, A extends Parameters<V>>(
   verb: V,
   args: A
 ): Promise<any> {
-  return new Promise((resolve, reject) => {
-    try {
-      interceptJsonResponse(context.response, (data) => {
-        resolve(data)
-      })
-
-      // @ts-ignore
-      verb(...args)(context)
-    } catch (error) {
-      reject(error)
-    }
+  return new Promise((resolve) => {
+    interceptJsonResponse(context.response, resolve)
+    ;(verb as any)(...args)(context).catch((err: any) => error(context.response, err))
   })
 }
